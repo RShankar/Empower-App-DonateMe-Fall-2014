1c83530a82fd8b83b1d72a5eeea932520efcb540
Seechay <cmandy@fau.edu>
2014-11-15 00:50:43 -0500

    Modified Base theme

--- Donate Me/src/edu/fau/group4/donateme/MainActivity.java
+++ Donate Me/src/edu/fau/group4/donateme/MainActivity.java
@@ -1,9 +1,7 @@
 package edu.fau.group4.donateme;
 
-import com.alexandre.onetraining.R;
-
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 //import android.widget.TextView;
