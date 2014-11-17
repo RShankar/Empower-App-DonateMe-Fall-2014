d5ec14e9817067781347c0740906afe1704bff40
asouzasantos2014 <asouzasantos2014@fau.edu>
2014-11-17 14:27:12 -0500

    Added activities

---
+++ Donate Me/src/edu/fau/group4/donateme/Login.java
@@ -0,0 +1,16 @@
+package com.alexandre.onetraining;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class Login extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.screen3);
+
+}
+}
---
+++ Donate Me/src/edu/fau/group4/donateme/Signin.java
@@ -0,0 +1,25 @@
+package com.alexandre.onetraining;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class Signin extends Activity implements OnClickListener{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.screen4);
+		
+		// Set up click listeners for all the buttons
+	    
+    }
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+}
